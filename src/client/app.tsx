102c584740bd86b9521a690604b043d330efd7a9
hughfenghen <hughfenghen@gmail.com>
2019-08-11 23:53:42 +0800

    创建内容编辑模块文件

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import ApiRecords from './api-records';
+import HttpContentPanel from './http-content-panel';
 
 function App () {
 
   return <div>
-
     <ApiRecords></ApiRecords>
+    <HttpContentPanel></HttpContentPanel>
   </div>
 }
 
---
+++ src/client/http-content-panel.jsx
@@ -0,0 +1,8 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+export default function HttpContentPanel () {
+  
+  return <section>
+    11111
+  </section>
+}
