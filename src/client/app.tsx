8a54df566aa59322bba5ec5396be1cfaba2d6a4e
hughfenghen <hughfenghen@gmail.com>
2019-08-20 09:15:49 +0800

    解决antd tab高度100%问题；支持ts 引入less module

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,13 +1,22 @@
 import 'antd/dist/antd.css';
+import s from './style.less';
 
+import { Tabs } from 'antd';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-
 import ApiRecords from './api-records';
 
-function App () {
-  return <div>
-    <ApiRecords></ApiRecords>
+
+function App() {
+  return <div className={s.app}>
+    <Tabs defaultActiveKey="snippet">
+      <Tabs.TabPane tab="Network" key="network">
+        <ApiRecords></ApiRecords>
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="Snippet" key="snippet">
+        <ApiRecords></ApiRecords>
+      </Tabs.TabPane>
+    </Tabs>
   </div>
 }
 
