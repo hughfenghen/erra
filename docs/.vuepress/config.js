fc70b4c3ee56435a1934930f4ff27fbdc074a709
hughfenghen <hughfenghen@gmail.com>
2019-11-24 15:59:35 +0800

    文档添加博客链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,13 @@ module.exports = {
     nav: [{
       text: 'GitHub',
       link: 'https://github.com/hughfenghen/erra',
+      target: '_self',
+      rel: '',
+    }, {
+      text: '风痕的博客',
+      link: 'https://hughfenghen.github.io/',
+      target: '_self',
+      rel: '',
     }],
     sidebarDepth: 2,
     sidebar: [
